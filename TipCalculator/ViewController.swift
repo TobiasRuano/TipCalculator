8dc2045855ea5a5fdf9c9f57eadac7e9d0f001d9
Tobias Ruano <ruano.t10@gmail.com>
2018-07-14 22:57:32 -0300

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Tobias Ruano on 14/7/18.
+//  Copyright © 2018 Tobias Ruano. All rights reserved.
+//
+
+import UIKit
+
+var price: Int = 0
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var initialPriceTextField: UITextField!
+    @IBOutlet weak var tipPriceLabel: UILabel!
+    @IBOutlet weak var totalPriceLabel: UILabel!
+    @IBOutlet weak var percentageTipLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+        if statusBar.responds(to:#selector(setter: UIView.backgroundColor)) {
+            statusBar.backgroundColor = UIColor(displayP3Red: 0.000, green: 0.478, blue: 1.000, alpha: 1.0)
+        }
+        UIApplication.shared.statusBarStyle = .default
+        
+        slider.setValue(0.15, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func SliderAction(_ sender: Any) {
+        initialPriceTextField.resignFirstResponder()
+        let value = Int((slider.value)*100)
+        percentageTipLabel.text = "Tip (\(value)%)"
+        
+        if price != 0 {
+            initialPriceTextField.text = "$\(price)"
+            let percentage = (price * value)/100
+        
+            tipPriceLabel.text = "$\(percentage)"
+            totalPriceLabel.text = "$\(String(percentage + price))"
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("return pressed")
+        textField.resignFirstResponder()
+        return false
+    }
+    
+}
+
+extension UITextField {
+    
+    @IBInspectable var doneAccessory: Bool {
+        get {
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+        if let prueba = Int(self.text!){
+            price = prueba
+            self.text = "$\(self.text!)"
+        }
+    }
+}
+
